ca7c03d3a543fa0ec92670c5d58433bfbef5027b
zhqstr <3186727428@qq.com>
2026-05-29 02:00:49 +0800

    OLED驱动适配：引脚改为PB5/PB6，添加OLED测试显示
    - OLED.c: 修改SCL/SDA引脚从PB8/PB9改为PB5/PB6
    - main.c: 显示ASCII测试信息验证OLED驱动

--- User/main.c
+++ User/main.c
@@ -1,22 +1,18 @@
 #include "stm32f10x.h"
 #include "Delay.h"
+#include "OLED.h"
 
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	OLED_Init();
+	OLED_Clear();
 
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_SetBits(GPIOC, GPIO_Pin_13);
+	OLED_ShowString(1, 1, "Smart Logistics");
+	OLED_ShowString(2, 1, "OLED Test OK!");
+	OLED_ShowString(3, 1, "Temp: 28.5 C");
+	OLED_ShowString(4, 1, "Humi: 65.3 %");
 
 	while (1)
 	{
-		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-		Delay_ms(500);
-		GPIO_SetBits(GPIOC, GPIO_Pin_13);
-		Delay_ms(500);
 	}
 }
